93322cd0fa412f8c06cf31507eb5b91ec660e81a
Daniel Cortés <hola@danielcortes.xyz>
2021-12-16 01:44:17 -0300

    link -> href

--- src/store.js
+++ src/store.js
@@ -4,38 +4,38 @@ const defaultGroups = [
   {
     name: 'General',
     links: [
-      {name: 'twitter', link: 'https://twitter.com'},
-      {name: 'youtube', link: 'https://youtube.com'},
-      {name: 'hacker_news', link: 'https://news.ycombinator.com'},
-      {name: 'lobsters', link: 'https://lobste.rs'},
-      {name: 'whatsapp', link: 'https://web.whatsapp.com'},
+      {name: 'twitter', href: 'https://twitter.com'},
+      {name: 'youtube', href: 'https://youtube.com'},
+      {name: 'hacker_news', href: 'https://news.ycombinator.com'},
+      {name: 'lobsters', href: 'https://lobste.rs'},
+      {name: 'whatsapp', href: 'https://web.whatsapp.com'},
     ]
   },
   {
     name: 'Inacap',
     links: [
-      {name: 'home', link: 'https://www.inacap.cl/'},
-      {name: 'intranet', link: 'https://adfs.inacap.cl/adfs/ls/?wtrealm=https://siga.inacap.cl/sts/&wa=wsignin1.0&wreply=https://siga.inacap.cl/sts/&wctx=https%3a%2f%2fadfs.inacap.cl%2fadfs%2fls%2f%3fwreply%3dhttps%3a%2f%2fwww.inacap.cl%2ftportalvp%2fintranet-alumno%26wtrealm%3dhttps%3a%2f%2fwww.inacap.cl%2f'},
-      {name: 'aprendizaje', link: 'https://www.inacap.cl/tportalvp/procesar_link.php?url=https://lms.inacap.cl/auth/saml2/login.php?wants=https://lms.inacap.cl/my/'},
+      {name: 'home', href: 'https://www.inacap.cl/'},
+      {name: 'intranet', href: 'https://adfs.inacap.cl/adfs/ls/?wtrealm=https://siga.inacap.cl/sts/&wa=wsignin1.0&wreply=https://siga.inacap.cl/sts/&wctx=https%3a%2f%2fadfs.inacap.cl%2fadfs%2fls%2f%3fwreply%3dhttps%3a%2f%2fwww.inacap.cl%2ftportalvp%2fintranet-alumno%26wtrealm%3dhttps%3a%2f%2fwww.inacap.cl%2f'},
+      {name: 'aprendizaje', href: 'https://www.inacap.cl/tportalvp/procesar_link.php?url=https://lms.inacap.cl/auth/saml2/login.php?wants=https://lms.inacap.cl/my/'},
     ]
   },
   {
     name: 'Reddit',
     links: [
-      {name: 'frontpage', link: 'https://reddit.com'},
-      {name: 'r/unixporn', link: 'https://reddit.com/r/unixporn'},
-      {name: 'r/piracy', link: 'https://reddit.com/r/piracy'},
-      {name: 'r/roms', link: 'https://reddit.com/r/roms'},
-      {name: 'r/chile', link: 'https://reddit.com/r/chile'},
+      {name: 'frontpage', href: 'https://reddit.com'},
+      {name: 'r/unixporn', href: 'https://reddit.com/r/unixporn'},
+      {name: 'r/piracy', href: 'https://reddit.com/r/piracy'},
+      {name: 'r/roms', href: 'https://reddit.com/r/roms'},
+      {name: 'r/chile', href: 'https://reddit.com/r/chile'},
     ]
   },
   {
     name: '4chan',
     links: [
-      {name: '/g/', link: 'https://4chan.org/g/catalog'},
-      {name: '/wg/', link: 'https://4chan.org/wg/catalog'},
-      {name: '/v/', link: 'https://4chan.org/v/catalog'},
-      {name: '/x/', link: 'https://4chan.org/x/catalog'},
+      {name: '/g/', href: 'https://4chan.org/g/catalog'},
+      {name: '/wg/', href: 'https://4chan.org/wg/catalog'},
+      {name: '/v/', href: 'https://4chan.org/v/catalog'},
+      {name: '/x/', href: 'https://4chan.org/x/catalog'},
     ]
   },
 ]
